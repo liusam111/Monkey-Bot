c20e4d3a33382f3e47302918b2ead6ff4ce3c23b
Sam <liusam111@gmail.com>
2020-07-25 22:31:30 -0700

    Refactored bot.js

--- bot.js
+++ bot.js
@@ -1,24 +1,23 @@
-//Define dependencies
+//Dependencies
 const fs = require("fs");
 const Discord = require("discord.js");
 const mysql = require("mysql");
 const {prefix, token, sqlpass} = require("./config.json");
 
-//Set up client
+//Constants
+const DEFAULT_COOLDOWN = 3;
+const SECS_TO_MS = 1000;
+const SECS_TO_MINS = 60000;
+VALID_STATUS = 200;
+
+//Discord client setup
 const client = new Discord.Client();
 client.commands = new Discord.Collection();
 client.active = new Discord.Collection();
 client.messageRepeat = new Discord.Collection();
 client.login(token);
 
-//Define constants
-const DEFAULT_COOLDOWN = 3;
-const FUN_FACT_COOLDOWN = 20;
-const SECS_TO_MS = 1000;
-const SECS_TO_MINS = 60000;
-VALID_STATUS = 200;
-
-//Get list of command files
+//Load commands from directory
 const commandFiles = fs.readdirSync("./commands").filter(file => file.endsWith(".js"));
 const cooldowns = new Discord.Collection();
 
@@ -29,7 +28,7 @@ for(const file of commandFiles){
     }
 }
 
-//Connect to MySQL Database
+//MySQL Setup
 var database = mysql.createConnection({
     host: "localhost",
     user: "root",
@@ -42,38 +41,24 @@ database.connect((err) => {
     console.log("Connected to Database!");
 });
 
-//Startup message, triggers once upon login
 client.once("ready", () => {
     console.log("Ready!");
-    const factcycle = require("./commands/automatic_facts.js");
-    setInterval(() => {
-        console.log("Fact Sent!");
-        [...client.guilds.values()].map((guild) => {
-            factcycle.execute(guild.id, client, database);
-        });
-    }, FUN_FACT_COOLDOWN * SECS_TO_MINS);
-
 });
 
-client.on("message", message => {
+client.on("message", async function(message) {
 
     //Don't allow bots to run commands
     if(message.author.bot) return;
 
-    //Increment exp
-    client.commands.get("incrementXP").execute(message, null, null, database);
-
     //Log messages
-    fs.appendFile("log.txt", ("[#" + message.channel.name + "] " + message.author.tag + ": " + message + "\n"), (err) => {
+    fs.appendFile("log.txt", (`[#${message.channel.name} (${message.channel.id})]  ${message.author.tag} (${message.author.id}): ${message}\n`), (err) => {
         if(err) console.error(err);
     });
 
-    const currentActive = client.active.has(message.author.id);
-
     //Exit early if user is in the middle of a Collector command
-    if(currentActive) return;
+    if(client.active.has(message.author.id)) return;
 
-    //Respond to mentions
+    //Easter egg for my personal servers
     if(message.content.match(/<@!?(651523467174346804)>/)){
         client.commands.get("who ping me").execute(message);
     }
@@ -83,45 +68,35 @@ client.on("message", message => {
         client.commands.get("no u").execute(message);
     }
 
-
-
     //Only run commands with prefix, but check repeat for messages without prefix
+    //TODO: Revamp this feature
     if(!message.content.startsWith(prefix)){
         client.commands.get("repeat").execute(message, null, client);
         return;
     }
 
-
-    console.log(message.content);
+    console.log(message.content); //For development purposes only
     
-    //Slice off prefix, split message by spacebars
+
+    //Parse message for command and args
     const original = message.content.slice(prefix.length).split(" ");
-
-    //When used in command execute functions, the args array values can be modified
     const args = message.content.toLowerCase().slice(prefix.length).split(/ +/);
-
-
     const commandName = args.shift();
     original.shift();
-
-
     const command = client.commands.get(commandName);
 
-    //Exit early if command doesn't exist or can't be run
     if(!command || command.cannotRun) return;
 
 
+
+    //Set cooldowns for commands
     if(!cooldowns.has(command.name)){
         cooldowns.set(command.name, new Discord.Collection());
     }
 
-
-    
-
     const now = Date.now();
     const timestamps = cooldowns.get(command.name);
-    //Either set as defined cooldown time or 3 as default (in secs)
-    const cooldownTime = (command.cooldown || DEFAULT_COOLDOWN) * SECS_TO_MS; 
+    const cooldownTime = (command.cooldown || DEFAULT_COOLDOWN) * SECS_TO_MS;
 
     if(timestamps.has(message.author.id)){
         const expireTime = timestamps.get(message.author.id) + cooldownTime;
@@ -132,35 +107,48 @@ client.on("message", message => {
         }
     } else {
         timestamps.set(message.author.id, now);
-        //Automatically delete user entry from timestamps after cooldown
+        //Automatically delete entry after cooldown
         setTimeout(() => timestamps.delete(message.author.id), cooldownTime);
     }
 
 
+    originalArgs = {
+        'message': message,
+        'args': original,
+        'client': client,
+        'database' : database
+    }
+
+    parsedArgs = {
+        'message': message,
+        'args': args,
+        'client': client,
+        'database' : database
+    }
 
 
-
-    //Run commands
+    //Command execution
     try{
-        if(command.needsOriginal){
-            command.execute(message, original, client, database);
-        } else if(command.limit_user){
-            client.active.set(message.author.id);
-            command.execute(message, args, client, database);
-        } else {
-            
-            //Check if command is only available on server text channels
-            if(command.guildOnly && message.channel.type !== 'text'){
-                return message.reply("Get that command out of my DMs.");
-            }
 
-            command.execute(message, args, client, database);
+        //Handle server only/non-DM commands
+        if(command.guildOnly && message.channel.type !== 'text'){
+            return message.reply("Get that command out of my DMs.");
         }
 
-    //Handle any errors gracefully
+        if(command.needsOriginal){
+            command.execute(originalArgs);
+
+        } else {
+            if(command.limitUser){
+                client.active.set(message.author.id);
+            }
+
+            await command.execute(parsedArgs);
+        }
+
     } catch(error) {
-        console.error(error);
-        message.reply("Error executing command");
+        console.error(error.message);
+        message.reply("Whoops! Something broke internally. Lemme just log this error...");
     }
     
 });
