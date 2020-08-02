c94075d7ee5602d86b24d5db0b5ec2f52172ac6f
Sam Liu <58928462+liusam111@users.noreply.github.com>
2020-08-02 02:05:44 -0700

    Delete test.js

--- test.js
+++
@@ -1,8 +0,0 @@
-
-var d = new Date("5/6/2019  4: pm ");
-console.log(d.toString());
-console.log(d.getTime() / 1000);
-console.log(Date.now() / 1000);
-
-const helper = require("./commands/helper/helper_remind.js");
-
